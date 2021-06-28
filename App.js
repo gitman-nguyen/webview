2b4ca4629e71667a411bf098188c2fc074c5dbb5
Chi.Nguyen <mr.nhchi@gmail.com>
2021-06-28 22:53:02 +0700

    added fixed WebView

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, StatusBar, Rectangle } from 'react-native';
+import { WebView } from 'react-native-webview';
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    //   <Text style={styles.redtext}>Youtube Player</Text>
+    // </View >
+    <View style={{
+      width: '100%',
+      height: 300,
+      paddingTop: StatusBar.currentHeight,
+      alignItems: 'center'
+    }}>
+      <View style={{
+        backgroundColor: 'yellow',
+        width: '100%',
+        height: 200
+      }}>
+        <WebView
+          source={{ uri: 'https://www.youtube.com/embed/8zT6CYu0iYQ' }}
+        />
+      </View>
+
+
+      <Text>Youtube videos</Text>
     </View>
   );
 }
@@ -16,6 +35,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: StatusBar.currentHeight,
+  },
+  redtext: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
